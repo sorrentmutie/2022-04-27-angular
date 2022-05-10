6025cce3d5b13d2eafe749ea6fdeb5984eecedfb
salvatore <salvatore.sorrentino@live.com>
2022-05-10 19:43:43 +0200

    preparazione lezione finale

--- my-demo/src/environments/environment.prod.ts
+++ my-demo/src/environments/environment.prod.ts
@@ -3,5 +3,7 @@ export const environment = {
   productsApiUrl: "http://localhost:7000/products",
   randomApiUrl: "https://randomuser.me/api?results=",
   reqresUrl: "https://reqres.in/api/users?page=2",
-  loginServer: "https://reqres.in/api/login"
+  loginServer: "https://reqres.in/api/login",
+  placeholder: "https://jsonplaceholder.typicode.com/users?q=",
+  toDo: "https://jsonplaceholder.typicode.com/todos"
 };
